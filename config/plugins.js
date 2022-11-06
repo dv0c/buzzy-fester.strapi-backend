323ee069b98e32e138f1078c860c0fbb15be8ca3
dv0c <50836855+dv0c@users.noreply.github.com>
2022-11-06 20:38:43 +0200

    modified:   config/middlewares.js 	modified:   config/plugins.js 	modified:   package-lock.json 	modified:   package.json

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,27 @@
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   'users-permissions': {
     config: {
       jwt: {
         expiresIn: '3d',
       },
       ratelimit: {
-        interval: 60000,
-        max: 100000000
+        interval: 100,
+        max: 100
       },
     },
   },
